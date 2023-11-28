f158d49125e24ff2fe679d179d16e8e0bc060a1d
Samuel L P Duque <131277691+Samuel-Duque@users.noreply.github.com>
2023-11-27 22:45:03 -0300

    Update FINAL DA agenda_contatos.c

--- agenda_contatos.c
+++ agenda_contatos.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
+#include <ctype.h>
+#define TAMANHO_MAX 200
 
 //estrutura do contato
 typedef struct contato{
     struct contato *anterior;
     struct contato *proximo;
+    int ID;
     char nome[50];
     char telefone[20];
     char email[50];
@@ -81,9 +84,27 @@ Contato* removerFim(Deque *deque) {
 //função basica p adicionar um contato
 void adicionarContato(Deque *deque) {
     Contato *contato = malloc(sizeof(Contato));
+
+    int maiorid = 0;
+    // declarar a variável ponteiro e inicializar ela com o endereço do primeiro do deque
+    Contato *atual = deque->inicio;
+    //percorre o deque para encontrar o maior id até o momento
+    while (atual != NULL)
+    {
+        if (atual->ID > maiorid)
+        {
+            maiorid = atual->ID;
+        }
+        atual = atual->proximo;
+        
+    }
+
+    contato->ID = maiorid + 1;
+
     printf("\n\n========================");
     printf("\n   ADICIONAR CONTATOS\n");
     printf("========================\n");
+
     printf("\nDigite o nome do contato: ");
     fgets(contato->nome, sizeof(contato->nome), stdin);
     contato->nome[strcspn(contato->nome, "\n")] = 0;
@@ -108,20 +129,28 @@ void adicionarContato(Deque *deque) {
         printf("Erro ao abrir o arquivo.\n");
         return;
     }
-    fprintf(arquivo,"%s,%d,%s,%s", contato->nome,contato->telefone,contato->email,contato->endereco);
+    fprintf(arquivo, "%d,%s,%s,%s,%s\n", contato->ID, contato->nome, contato->telefone, contato->email, contato->endereco);
     fclose(arquivo);
 }
-//função para adicionar contato no csv
-void adicionarContatoCSV(Contato *contato){
-    FILE *arquivo;
-    arquivo = fopen("contatos.csv","a");
-
-    if(arquivo == NULL){
-        printf("Erro ao abrir o arquivo.\n");
+//função para criar um arquivo csv
+void criarArquivocsv(){
+    FILE *arquivo = fopen("contatos.csv","r");
+    if(arquivo != NULL){
+        fclose(arquivo);
+        printf("O arquivo csv já existe, então não é necessário criar um novo.\n");
         return;
     }
-    fprintf(arquivo,"%s,%d,%s,%s")
+    arquivo = fopen("contatos.csv","w");
+    if (arquivo == NULL){
+        printf("Erro ao criar o arquivo csv.\n");
+        return;
+    }
+    fprintf(arquivo,"ID, Nome ,Telefone ,Email ,Endereco\n");
+
+    fclose(arquivo);
+    printf("Arquivo csv criado com sucesso.\n");
 }
+
 void imprimirContatos(Deque *deque) {
     Contato *contato = deque->inicio;
         printf("\n\n    ==============================");
@@ -130,6 +159,7 @@ void imprimirContatos(Deque *deque) {
         printf("Nome     Número     Endereço     Email\n");
         printf("=======================================\n");
     while (contato != NULL) {
+        printf("ID: %d",contato->ID);
         printf("\nNome: %s\n", contato->nome);
         printf("Telefone: %s\n", contato->telefone);
         printf("Email: %s\n", contato->email);
@@ -141,7 +171,7 @@ void imprimirContatos(Deque *deque) {
 
 void buscarContato(Deque *deque) {
   char contatoBusca[1000];
-  int escolhaMenu;
+//   int escolhaMenu;
   Contato *contato = deque->inicio;
   
   if (contato==NULL){
@@ -159,8 +189,9 @@ void buscarContato(Deque *deque) {
             printf("\n\n    ==============================");
             printf("\n              CONTATO %s  \n", contato->nome);
             printf("    ==============================\n\n");
-            printf("Nome     Número     Endereço     Email\n");
+            printf("ID     Nome     Número     Endereço     Email\n");
             printf("=======================================\n");
+            printf("\nID: %d",contato->ID);
             printf("\nNome: %s", contato->nome);
             printf("\nTelefone: %s", contato->telefone);
             printf("\nEmail: %s", contato->email);
@@ -172,17 +203,115 @@ void buscarContato(Deque *deque) {
         printf("Contato não encontrado.\n");
       }                                              
 }
+void removerContato(Deque *deque, int id) {
+    //armazena o contato que erá removido
+    Contato *contatoRemovido = NULL;
+    //criei um ponteiro para o contato atual que esta no deque
+    Contato *atual = deque->inicio;
+    //criei outro ponteiro para o anterior ao atual
+    Contato *anterior = NULL;
 
+    // Lista encadeada para armazenar contatos não removidos
+    Contato *contatosNaoRemovidos = NULL;
+    //ponteiro para o ultimo contato para adicionar contatos no final
+    Contato *ultimoContato = NULL;
 
-void removerContato(Deque *deque) {
-    Contato *contato = removerInicio(deque);
-    if (contato != NULL) {
-        printf("Removido o contato %s\n", contato->nome);
-        free(contato);
-    } else {
-        printf("A agenda de contatos ja esta vazia.\n");
+    // para encontrar o contato no deque pelo ID
+    while (atual != NULL && atual->ID != id) {
+        anterior = atual;
+        atual = atual->proximo;
     }
+
+    // remove o contato no deque
+    if (atual != NULL) {
+        if (anterior != NULL) {
+            anterior->proximo = atual->proximo;
+        } else {
+            deque->inicio = atual->proximo;
+        }
+
+        if (atual == deque->fim) {
+            deque->fim = anterior;
+        }
+
+        contatoRemovido = atual;
+        //checagem
+        printf("Contato com ID %d removido do deque com sucesso!\n", id);
+
+    } else {//checagem 
+        printf("Contato com ID %d não encontrado no deque.\n", id);
+        return;
+    }
+
+    //leitura do arquivo
+    FILE *arquivo = fopen("contatos.csv", "r");
+    if (arquivo == NULL) {//checagem
+        printf("Erro em abrir o arquivo.\n");
+        return;
+    }
+
+    char linha[512];
+    //para indicar se o cabecalho esta na primeira linha!
+    int cabecalho = 1;
+    // le uma linha inteira do arquivo, até encontrar uma quebra de linha ou o fim do arquivo
+    while (fgets(linha, sizeof(linha), arquivo)) {
+        if (cabecalho) {
+            // Mantém o cabeçalho
+            cabecalho = 0;
+        } else {
+            // cria um novo contato e preenche com os dados da linha
+            Contato *novoContato = malloc(sizeof(Contato));
+            sscanf(linha, "%d,%[^,],%[^,],%[^,],%[^\n]", &novoContato->ID, novoContato->nome, novoContato->telefone, novoContato->email, novoContato->endereco);
+
+            // Compara os IDs para ver se o id no novo é diferente do id que quer remover
+            if (novoContato->ID != id) {
+                // Se não é o ID que queremos remover, adiciona a lista encadeada
+                novoContato->proximo = NULL;
+                if (contatosNaoRemovidos == NULL) {
+                    contatosNaoRemovidos = novoContato;
+                    ultimoContato = novoContato;
+                } else {
+                    ultimoContato->proximo = novoContato;
+                    ultimoContato = novoContato;
+                }
+            }
+        }
+    }
+
+    fclose(arquivo);
+
+    // Abre o arquivo para escrever
+    arquivo = fopen("contatos.csv", "w");
+    if (arquivo == NULL) {
+        printf("Erro em abrir o arquivo para escrita.\n");
+        return;
+    }
+
+    // Escreve o cabeçalho no arquivo
+    fprintf(arquivo, "ID,Nome,Telefone,Email,Endereco\n");
+
+    // Escreve os contatos não removidos no arquivo
+    Contato *temp = contatosNaoRemovidos;
+    //para escrever todos os contatos na lista sem o q removeu 
+    while (temp != NULL) {
+        fprintf(arquivo, "%d,%s,%s,%s,%s\n", temp->ID, temp->nome, temp->telefone, temp->email, temp->endereco);
+        Contato *proximo = temp->proximo;
+        free(temp); // Libera a memória do contato
+        temp = proximo;
+    }
+
+    fclose(arquivo);
+
+    printf("Contato com ID %d removido do arquivo com sucesso!\n", id);
 }
+
+
+
+
+
+
+
+
 
 void editarContato(Deque *deque){
     char contatoBusca[1000];
@@ -250,8 +379,9 @@ void editarContato(Deque *deque){
 int main() {
     Deque agenda;
     inicializar(&agenda);
-
+    criarArquivocsv();
     int opcao;
+    int idcontato;
     do {
         printf("\n\n    AGENDA DE CONTATOS\n");
         printf("=========================");
@@ -277,7 +407,10 @@ int main() {
                 buscarContato(&agenda);
                 break;
             case 4:
-                removerContato(&agenda);
+                imprimirContatos(&agenda);
+                printf("Insira o ID do contato para remover: ");
+                scanf("%d", &idcontato);
+                removerContato(&agenda, idcontato);
                 break;
             case 5:
                 editarContato(&agenda);
